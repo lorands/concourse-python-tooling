0a880949f45bb608e8dc30785ef235b947e295b3
lorands <lorand.somogyi@plensys.com>
2018-10-05 09:18:30 +0200

    feat(logging): Add some more logging and cleanup
    Closes:

--- test/snippet.py
+++ test/snippet.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
-# from cloudfoundry_client.client import CloudFoundryClient
-# from concourse_python_tooling.concourse_tooling.bx import bx_utils
+import os
+import logging
 from concoursetooling.cf.cloud_foundry import CloudFoundry
+from concoursetooling.bx import bx_utils
+
+logging.getLogger().setLevel(logging.DEBUG)
+lr = bx_utils.bx_login(
+    os.environ['BX_API'], \
+    os.environ['BX_USER'], \
+    os.environ['BX_PASS'], \
+    os.environ['BX_ACC'], \
+    os.environ['BX_RESOURCE_GROUP'] )
 
 ss = CloudFoundry.get_all_services()
 
